43b9c7a4a995f70e51438e0396d54ae274b10a2c
jiazihan <jiazihan.2034@bytedance.com>
2020-07-07 00:07:18 +0800

    feat: 实现懒加载

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -28,7 +28,7 @@ module.exports = merge(baseWebpackConfig, {
     proxy: [
       {
         context: ["/v1"],
-        target: "http://127.0.0.1:8081",
+        target: "http://localhost:8081",
         changeOrigin: true,
       },
     ],
